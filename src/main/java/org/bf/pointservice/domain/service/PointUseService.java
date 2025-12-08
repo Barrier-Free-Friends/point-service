74ddb5c4a78990ad74bfdfdeb64ab10ab7378942
jinnk0 <jin8073@gmail.com>
2025-12-08 13:24:52 +0900

    feat: PointBalance 도메인 로직 정의 (#4) - 포인트 획득, 사용, 취소 도메인 서비스 인터페이스 정의 - 뱃지 업데이트를 위한 인터페이스 정의

---
+++ src/main/java/org/bf/pointservice/domain/service/PointUseService.java
@@ -0,0 +1,11 @@
+package org.bf.pointservice.domain.service;
+
+import java.util.UUID;
+
+public interface PointUseService {
+    /**
+     * 포인트 사용 프로세스 전체를 처리
+     * TODO: 잔액 차감, 보상 재고 감소, History 기록 생성
+     */
+    void usePoints(UUID userId, long points);
+}
