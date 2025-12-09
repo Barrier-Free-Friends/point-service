cad220fcdc47fdc8af8f56f15772b458e72e9418
jinnk0 <jin8073@gmail.com>
2025-12-09 16:26:13 +0900

    feat: point 사용 도메인 서비스에 보상 획득 내용 추가 (#5)

--- src/main/java/org/bf/pointservice/domain/exception/reward/RewardErrorCode.java
+++ src/main/java/org/bf/pointservice/domain/exception/reward/RewardErrorCode.java
@@ -11,7 +11,8 @@ public enum RewardErrorCode implements BaseErrorCode {
     INVALID_PRICE(HttpStatus.BAD_REQUEST, "PRICE400", "보상 구매를 위한 포인트는 음수가 될 수 없습니다."),
     INVALID_REWARD_NAME(HttpStatus.BAD_REQUEST, "REWARD_NAME_400", "보상 이름은 필수입니다."),
     INVALID_RELATION(HttpStatus.BAD_REQUEST, "RELATION400", "유저 아이디와 보상 아이디는 필수 입니다."),
-    ALREADY_USED(HttpStatus.BAD_REQUEST, "STATUS400", "이미 사용한 보상입니다.");
+    ALREADY_USED(HttpStatus.BAD_REQUEST, "STATUS400", "이미 사용한 보상입니다."),
+    REWARD_NOT_FOUND(HttpStatus.NOT_FOUND, "REWARD404", "해당하는 보상을 찾을 수 없습니다.");
 
     private final HttpStatus status;
     private final String code;
--- src/main/java/org/bf/pointservice/domain/repository/reward/RewardRepository.java
+++ src/main/java/org/bf/pointservice/domain/repository/reward/RewardRepository.java
@@ -3,7 +3,9 @@ package org.bf.pointservice.domain.repository.reward;
 import org.bf.pointservice.domain.entity.reward.Reward;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface RewardRepository extends JpaRepository<Reward, UUID> {
+    Optional<Reward> findByRewardId(UUID rewardId);
 }
