59288cfcbfb7d69041ff181f7d79e74634ed54c9
jinnk0 <jin8073@gmail.com>
2025-12-10 09:17:08 +0900

    feat: 뱃지 조회 기능 구현 (#9)

--- src/main/java/org/bf/pointservice/domain/repository/badge/BadgeRepository.java
+++ src/main/java/org/bf/pointservice/domain/repository/badge/BadgeRepository.java
@@ -1,6 +1,8 @@
 package org.bf.pointservice.domain.repository.badge;
 
 import org.bf.pointservice.domain.entity.badge.Badge;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -9,4 +11,5 @@ import java.util.UUID;
 public interface BadgeRepository extends JpaRepository<Badge, UUID> {
     Optional<Badge> findByBadgeId(UUID badgeId);
     boolean existsByMinPoint(long minPoint);
+    Page<Badge> findAllByDeletedAtIsNull(Pageable pageable);
 }
