6e5782d966fbf1944435fd3a8f31fb42e34e39f0
jinnk0 <jin8073@gmail.com>
2025-12-10 09:12:35 +0900

    feat: 뱃지 삭제 기능 구현 (#9)

--- src/main/java/org/bf/pointservice/application/command/BadgeCommandService.java
+++ src/main/java/org/bf/pointservice/application/command/BadgeCommandService.java
@@ -3,7 +3,12 @@ package org.bf.pointservice.application.command;
 import org.bf.pointservice.application.dto.BadgeCreateRequest;
 import org.bf.pointservice.application.dto.BadgeResponse;
 
+import java.util.UUID;
+
 public interface BadgeCommandService {
     // 신규 뱃지 생성
     BadgeResponse createBadge(BadgeCreateRequest request);
+
+    // 뱃지 삭제
+    void deleteBadge(UUID badgeId);
 }
--- src/main/java/org/bf/pointservice/application/command/impl/BadgeCommandServiceImpl.java
+++ src/main/java/org/bf/pointservice/application/command/impl/BadgeCommandServiceImpl.java
@@ -2,13 +2,18 @@ package org.bf.pointservice.application.command.impl;
 
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.bf.global.infrastructure.exception.CustomException;
+import org.bf.global.security.SecurityUtils;
 import org.bf.pointservice.application.command.BadgeCommandService;
 import org.bf.pointservice.application.dto.BadgeCreateRequest;
 import org.bf.pointservice.application.dto.BadgeResponse;
 import org.bf.pointservice.domain.entity.badge.Badge;
+import org.bf.pointservice.domain.exception.badge.BadgeErrorCode;
 import org.bf.pointservice.domain.repository.badge.BadgeRepository;
 import org.bf.pointservice.domain.service.CheckPointGap;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 @Transactional
@@ -17,6 +22,7 @@ public class BadgeCommandServiceImpl implements BadgeCommandService {
 
     private final BadgeRepository badgeRepository;
     private final CheckPointGap checkPointGap;
+    private final SecurityUtils securityUtils;
 
     @Override
     public BadgeResponse createBadge(BadgeCreateRequest request) {
@@ -29,5 +35,13 @@ public class BadgeCommandServiceImpl implements BadgeCommandService {
                 .build();
         badgeRepository.save(badge);
         return BadgeResponse.from(badge);
+    }
+
+    @Override
+    public void deleteBadge(UUID badgeId) {
+        Badge badge = badgeRepository.findByBadgeId(badgeId).orElseThrow(
+                () -> new CustomException(BadgeErrorCode.BADGE_NOT_FOUND)
+        );
+        badge.softDelete(securityUtils.getCurrentUsername());
     }
 }
