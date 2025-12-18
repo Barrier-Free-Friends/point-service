ccd2a12f65ad24af025edcbf53ef2f300a583b9d
jinnk0 <jin8073@gmail.com>
2025-12-18 15:15:39 +0900

    feat: 전수 유저 뱃지 정보 업데이트를 위한 비동기 배치 시스템 스케줄러 구현

---
+++ src/main/java/org/bf/pointservice/infrastructure/batch/BadgeUpdateScheduler.java
@@ -0,0 +1,34 @@
+package org.bf.pointservice.infrastructure.batch;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+@EnableScheduling
+public class BadgeUpdateScheduler {
+
+    private final JobLauncher asyncJobLauncher;
+    private final Job updateBadgeJob;
+
+    @Scheduled(cron = "0 0 3 * * *")
+    public void updateBadge() {
+        try {
+            JobParameters params = new JobParametersBuilder()
+                    .addLong("time", System.currentTimeMillis())
+                    .toJobParameters();
+            asyncJobLauncher.run(updateBadgeJob, params);
+            log.info("뱃지 업데이트 배치 작업이 시작되었습니다.");
+        } catch (Exception e) {
+            log.error("뱃지 업데이트 배치 작업 중 에러 발생 : ", e);
+        }
+    }
+}
