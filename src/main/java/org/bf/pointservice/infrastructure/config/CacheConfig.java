2bcb6c3c6a17c026ac4f08da6d378788a9e251d2
jinnk0 <jin8073@gmail.com>
2025-12-23 13:13:00 +0900

    add: Caffeine 의존성 및 캐시 설정 추가 (#48)

---
+++ src/main/java/org/bf/pointservice/infrastructure/config/CacheConfig.java
@@ -0,0 +1,33 @@
+package org.bf.pointservice.infrastructure.config;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+    @Bean
+    public CacheManager cacheManager() {
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager();
+
+        cacheManager.setCacheNames(Arrays.asList("badgeVersion", "badges"));
+
+        cacheManager.setCaffeine(caffeineCacheBuilder());
+        return cacheManager;
+    }
+
+    Caffeine<Object, Object> caffeineCacheBuilder() {
+        return Caffeine.newBuilder()
+                .initialCapacity(100)
+                .maximumSize(500)
+                .expireAfterWrite(10, TimeUnit.MINUTES) // 10분 후 만료
+                .recordStats();
+    }
+}
